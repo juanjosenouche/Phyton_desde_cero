745f044c626eb492b3ad5cac89a61b88a6364108
juanjosenouche <43372848+juanjosenouche@users.noreply.github.com>
2022-10-27 23:30:11 +0200

    Add files via upload
    Entrada_de_datos

---
+++ Entrada_de_datos.py
@@ -0,0 +1,13 @@
+#entrada de datos
+
+#el input lo guarda en "nombre" como tipo "string"
+nombre = input("Digite su nombre: ")
+
+print(f"Hola {nombre}")
+
+#guarda tipos numéricos
+#int float
+
+numero = int(input("Digite un valor numérico: "))
+
+print (f"El número es {numero}")
